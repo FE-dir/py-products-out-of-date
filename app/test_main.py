b62bdd457625a712ff3c3cb042ffd56da2f419ce
Fedir <fedor.agureev@gmail.com>
2025-02-10 21:20:29 +0200

    fixes

--- app/test_main.py
+++ app/test_main.py
@@ -32,13 +32,15 @@ def test_outdated_products(sample_products: List[Dict[str, Any]]) -> None:
         assert main.outdated_products(sample_products) == ["duck"]
 
 
-def test_outdated_products_no_expired(sample_products: List[Dict[str, Any]]) -> None:
+def test_outdated_products_no_expired(
+        sample_products: List[Dict[str, Any]]) -> None:
     with patch("app.main.datetime") as mock_datetime:
         mock_datetime.date.today.return_value = datetime.date(2022, 1, 31)
         assert main.outdated_products(sample_products) == []
 
 
-def test_outdated_products_all_expired(sample_products: List[Dict[str, Any]]) -> None:
+def test_outdated_products_all_expired(
+        sample_products: List[Dict[str, Any]]) -> None:
     with patch("app.main.datetime") as mock_datetime:
         mock_datetime.date.today.return_value = datetime.date(2023, 1, 1)
         assert main.outdated_products(sample_products) == [
@@ -52,7 +54,8 @@ def test_expiration_day_today_not_outdated(
     sample_products: List[Dict[str, Any]]
 ) -> None:
     with patch("app.main.datetime") as mock_datetime:
-        mock_datetime.date.today.return_value = datetime.date(2022, 2, 10)
+        mock_datetime.date.today.return_value = datetime.date(
+            2022, 2, 10)
         assert main.outdated_products(sample_products) == ["chicken", "duck"], (
             "Product with expiration date equal to today should NOT be outdated."
         )
