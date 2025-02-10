6f5bae6de3d6c5d46951790cdf753b408bbd9948
Fedir <fedor.agureev@gmail.com>
2025-02-10 21:16:56 +0200

    fixes

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,6 @@
 import datetime
 import pytest
-from pytest_mock import MockerFixture
+from unittest.mock import patch
 import app.main as main
 from typing import List, Dict, Any
 
@@ -26,39 +26,31 @@ def sample_products() -> List[Dict[str, Any]]:
     ]
 
 
-def test_outdated_products(
-    mocker: MockerFixture, sample_products: List[Dict[str, Any]]
-) -> None:
-    mock_date = mocker.patch("app.main.datetime.date", wraps=datetime.date)
-    mock_date.today.return_value = datetime.date(2022, 2, 2)
-    assert main.outdated_products(sample_products) == ["duck"]
+def test_outdated_products(sample_products: List[Dict[str, Any]]) -> None:
+    with patch("app.main.datetime") as mock_datetime:
+        mock_datetime.date.today.return_value = datetime.date(2022, 2, 2)
+        assert main.outdated_products(sample_products) == ["duck"]
 
 
-def test_outdated_products_no_expired(
-    mocker: MockerFixture, sample_products: List[Dict[str, Any]]
-) -> None:
-    mock_date = mocker.patch("app.main.datetime.date", wraps=datetime.date)
-    mock_date.today.return_value = datetime.date(2022, 1, 31)
-    assert main.outdated_products(sample_products) == []
+def test_outdated_products_no_expired(sample_products: List[Dict[str, Any]]) -> None:
+    with patch("app.main.datetime") as mock_datetime:
+        mock_datetime.date.today.return_value = datetime.date(2022, 1, 31)
+        assert main.outdated_products(sample_products) == []
 
 
-def test_outdated_products_all_expired(
-    mocker: MockerFixture, sample_products: List[Dict[str, Any]]
-) -> None:
-    mock_date = mocker.patch("app.main.datetime.date", wraps=datetime.date)
-    mock_date.today.return_value = datetime.date(2023, 1, 1)
-    assert main.outdated_products(sample_products) == [
-        "salmon",
-        "chicken",
-        "duck",
-    ]
+def test_outdated_products_all_expired(sample_products: List[Dict[str, Any]]) -> None:
+    with patch("app.main.datetime") as mock_datetime:
+        mock_datetime.date.today.return_value = datetime.date(2023, 1, 1)
+        assert main.outdated_products(sample_products) == [
+            "salmon",
+            "chicken",
+            "duck",
+        ]
 
 
-def test_expiration_day_today_not_outdated(
-    mocker: MockerFixture, sample_products: List[Dict[str, Any]]
-) -> None:
-    mock_date = mocker.patch("app.main.datetime.date", wraps=datetime.date)
-    mock_date.today.return_value = datetime.date(2022, 2, 10)
-    assert main.outdated_products(sample_products) == ["chicken", "duck"], (
-        "Product with expiration date equal to today should NOT be outdated."
-    )
+def test_expiration_day_today_not_outdated(sample_products: List[Dict[str, Any]]) -> None:
+    with patch("app.main.datetime") as mock_datetime:
+        mock_datetime.date.today.return_value = datetime.date(2022, 2, 10)
+        assert main.outdated_products(sample_products) == ["chicken", "duck"], (
+            "Product with expiration date equal to today should NOT be outdated."
+        )
